995eac537956c6070aac8de17edb09618b160df4
Shaban Irshad <shabanirshad408@gmail.com>
2026-02-16 13:39:15 +0530

    added8

--- src/components/UserDetails.js
+++ src/components/UserDetails.js
@@ -1,36 +1,33 @@
-import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
-const UserDetails = () => {
-  const { id } = useParams();
-  const [user, setUser] = useState(null);
-
-  useEffect(() => {
-    setUser(null);   // IMPORTANT: reset state immediately
-
-    fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
-      .then(res => res.json())
-      .then(data => {
-        setUser(data);
-      })
-      .catch(err => console.log(err));
-
-  }, [id]);
-
-  if (!user) {
-    return <div>Loading...</div>;   // Cypress expects this
-  }
-
-  return (
-    <div>
-      <h1>User Details</h1>
-      <p>Name: {user.name}</p>
-      <p>Username: {user.username}</p>
-      <p>Email: {user.email}</p>
-      <p>Phone: {user.phone}</p>
-      <p>Website: {user.website}</p>
-    </div>
-  );
-};
-
+const UserDetails=()=>{
+    const {id}=useParams();
+    const [user,setUser]=useState({});
+    const [loading,setLoading]=useState(true);
+    console.log(id);
+    useEffect(()=>{
+        setLoading(true);
+        fetch(`https://jsonplaceholder.typicode.com/users/${id}`).then(res=>res.json()).then(data=>{
+            setUser(data);
+            setLoading(false);
+        }).catch(err=>console.log(err));
+    },[id])
+    console.log(user);
+    return (
+        <>  
+            {loading?<div>Loading...</div>:
+                <div>
+                     <h1>User Details </h1>
+                     <p>Name: {user.name}</p>
+                     <p>Username: {user.username}</p>
+                     <p>Email: {user.email}</p>
+                     <p>Phone: {user.phone}</p>
+                     <p>Website: {user.website}</p>
+                </div>
+            }
+           
+        </>
+    );
+}
 export default UserDetails;
