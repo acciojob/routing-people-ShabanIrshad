52824a043c9893d98a33e835e80b5a1bf0769bae
Shaban Irshad <shabanirshad408@gmail.com>
2026-02-20 12:25:05 +0530

    added

--- src/components/UserDetails.js
+++ src/components/UserDetails.js
@@ -1,33 +1,37 @@
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
-const UserDetails=()=>{
-    const {id}=useParams();
-    const [user,setUser]=useState({});
-    const [loading,setLoading]=useState(true);
-    console.log(id);
-    useEffect(()=>{
-        setLoading(true);
-        fetch(`https://jsonplaceholder.typicode.com/users/${id}`).then(res=>res.json()).then(data=>{
-            setUser(data);
-            setLoading(false);
-        }).catch(err=>console.log(err));
-    },[id])
-    console.log(user);
-    return (
-        <>  
-            {loading?<div><h1>Loading...</h1></div>:
-                <div>
-                     <h1>User Details </h1>
-                     <p>Name: {user.name}</p>
-                     <p>Username: {user.username}</p>
-                     <p>Email: {user.email}</p>
-                     <p>Phone: {user.phone}</p>
-                     <p>Website: {user.website}</p>
-                </div>
-            }
-           
-        </>
-    );
-}
+const UserDetails = () => {
+  const { id } = useParams();
+  const [user, setUser] = useState({});
+  const [loading, setLoading] = useState(true);
+  console.log(id);
+  useEffect(() => {
+    setLoading(true);
+    fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setUser(data);
+        setLoading(false);
+      })
+      .catch((err) => console.log(err));
+  }, [id]);
+  console.log(user);
+  return (
+    <>
+      {loading ? (
+        <div>Loading...</div>
+      ) : (
+        <div>
+          <h1>User Details </h1>
+          <p>Name: {user.name}</p>
+          <p>Username: {user.username}</p>
+          <p>Email: {user.email}</p>
+          <p>Phone: {user.phone}</p>
+          <p>Website: {user.website}</p>
+        </div>
+      )}
+    </>
+  );
+};
 export default UserDetails;
